51170c095bdce0f9028d10c7088afdb6a2c1e34d
Jaanus Salmh <jaanus.salm@gmail.com>
2025-09-19 10:59:49 +0300

    Minu esimene Giti projekt

---
+++ suvaline.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#see on minu esimene bashi skript, mis teeb midagi, millest ma midagi aru ei saa
+
+TERVITUS="see on tervitus AI poolt"
+
+
+echo "$TERVITUS"
+echo "Sina oled $(whoami)"
+echo" "Tore oli Sinuga tuttavaks saada!"
+
